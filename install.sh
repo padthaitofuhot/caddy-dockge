6f4edf5abc2526d438dfa5bf46263d63b163bf1a
padthaitofuhot <padthaitofuhot@users.noreply.github.com>
2025-03-01 18:41:35 -0500

    sausage

--- install.sh
+++ install.sh
@@ -8,4 +8,6 @@ sudo find "/opt/stacks" -type f | sudo xargs chmod -c 0640
 
 # Use Docker buildx bake to speed up builds
 export COMPOSE_BAKE=true
+
 sudo docker compose -f /opt/stacks/dockge/compose.yml up --build --pull always -d
+sudo docker compose -f /opt/stacks/dockge/compose.yml logs -f
--- stacks/dockge/rebuild.sh
+++ stacks/dockge/rebuild.sh
@@ -3,8 +3,10 @@ set -x
 
 # Use Docker buildx bake to speed up builds
 export COMPOSE_BAKE=true
+
 sudo docker compose -f /opt/stacks/dockge/compose.yml build
 sudo docker compose -f /opt/stacks/dockge/compose.yml pull
 sudo docker compose -f /opt/stacks/dockge/compose.yml down
 sudo docker compose -f /opt/stacks/dockge/compose.yml up -d
 sudo docker image prune -f
+sudo docker compose -f /opt/stacks/dockge/compose.yml logs -f
